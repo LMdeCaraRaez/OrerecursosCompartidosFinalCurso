260fb49c01cff32cc0e1691b56bc11226701942b
luismi <luismigueldecararaez@gmail.com>
2024-05-03 09:22:20 +0200

    Commit de cosas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment, useState} from "react";
+import {Route, Router, Routes, useNavigate} from 'react-router-dom';
+import Registrarse from "./paginas/registrarse";
+import Home from "./paginas/home";
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <Routes>
+            <Route path="/paginas/home" element={<Home/>}/>
+            <Route path="/paginas/registrarse" element={<Registrarse/>}/>
+        </Routes>
+    );
 }
 
 export default App;
